46b41d92740f899588252e4b30439de2b058128a
goldenbeardtesla <77920801+goldenbeardtesla@users.noreply.github.com>
2021-09-12 08:06:55 +0530

    latest upload

--- bin/dataingestion.py
+++ bin/dataingestion.py
@@ -1,39 +1,56 @@
 import os
+import flask
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
 
 class Ingest:
 
-    def findAllClass(basePath):
-
-        """to find the class names.
+    def findAllClass(self, basePath):
+        """To find the class names.
 
         Input:
-            path:A String for path of data
+            path: A string for path of data
         Output:
-            classArr:An Array of Strings(classnames)
+            classArr: An array of String(class names)"""
 
-        """
-        classArr = os.listdir(self,basePath)
-
+        classArr = os.listdir(basePath)
         return classArr
 
-    def readContentFromDir(self,basePath,className):
+    def readContentFromDir(self, basePath, className):
 
-        classDir = os.path.join(basePath,className)
+        classDir = os.path.join(basePath, className)
         files = os.listdir(classDir)
         contentList = []
         for file in files:
-            with open(os.path.join(classDir,file)) as fl:
+            with open(os.path.join(classDir, file)) as fl:
                 content = fl.read()
             contentList.append(content)
+        return contentList
 
-        return contentList:
-
-    def collectData(self):
+    def collectData(self, basePath):
 
         classNames = self.findAllClass(basePath)
         finalData = {}
         for className in classNames:
-            contentList = self.readContentFromDir(basePath,className)
+            contentList = self.readContentFromDir(basePath, className)
             finalData[className] = contentList
         return finalData
+
+
+@app.route('/fetch')
+def getData():
+    ingestObj = Ingest()
+    basePath = r'/Users/prathameshgarge/Desktop/Grey-Atom/GADeployment1/data/20news-18828'
+    finalData = ingestObj.collectData(basePath)
+    return json.dumps(finalData)
+
+
+app.run('localhost', 5000)
+
+
+
+
 
